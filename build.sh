7875ec4c8d901224d8df3030a35fadd961be83a1
Mattias Amnefelt <mattias.amnefelt@backlight.co>
2025-02-24 10:11:13 +0000

    Chown the workspace

--- build.sh
+++ build.sh
@@ -1,5 +1,7 @@
 #! /bin/bash -x
 
+sudo chown -R jenkins: $WORKSPACE || /bin/true
+
 docker build -t openresty-build .
 
 docker run -e "BUILD_NUMBER=${BUILD_NUMBER}" --rm -v "$PWD:$PWD" -w "$PWD" openresty-build make
